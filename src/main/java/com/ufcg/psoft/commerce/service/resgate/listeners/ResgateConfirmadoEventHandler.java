2b1bf8f60b1f862862ba15364b57638149162981
calebia <nivea.calebia.felix.santos@ccc.ufcg.edu.br>
2025-09-06 17:13:41 -0300

    notificacao resgate confirmado

---
+++ src/main/java/com/ufcg/psoft/commerce/service/resgate/listeners/ResgateConfirmadoEventHandler.java
@@ -0,0 +1,24 @@
+package com.ufcg.psoft.commerce.service.resgate.listeners;
+
+import com.ufcg.psoft.commerce.service.notificacao.NotificacaoService;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResgateConfirmadoEventHandler {
+    private final NotificacaoService notificacaoService;
+
+    public ResgateConfirmadoEventHandler(NotificacaoService notificacaoService) {
+        this.notificacaoService = notificacaoService;
+    }
+    @EventListener
+    public void handleResgateConfirmado(ResgateConfirmadoEvent event) {
+        var resgate = event.getResgate();
+        var cliente = resgate.getCliente();
+        String mensagem = String.format(
+                "Notificação: O resgate do ativo %s solicitado pelo cliente %s foi CONFIRMADO.%n",
+                resgate.getAtivo().getNome(), cliente.getNome());
+
+        notificacaoService.notificar(cliente, mensagem);
+    }
+}
